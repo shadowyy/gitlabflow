2ad9fada83e876ea793427322f321b709fe96948
GitHub Action <action@github.com>
2024-01-16 07:33:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/shadowyy/gitlabflow/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shadowyy/gitlabflow/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.shadowyy.gitlabflow.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shadowyy.gitlabflow.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/shadowyy/gitlabflow/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/shadowyy/gitlabflow/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.shadowyy.gitlabflow.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.shadowyy.gitlabflow.MyBundle
+import com.github.shadowyy.gitlabflow.services.MyProjectService
 import javax.swing.JButton
 
 
